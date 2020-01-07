aeba60f8c27d4d937560e34465eb36fbc1ddae3c
Javier Montalvo <giojavi04@gmail.com>
2020-01-07 17:20:09 -0500

    dinamic fixed position

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -4,6 +4,7 @@ import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(function () {
     window.fetch('https://petgram-server.midudev.now.sh/categories')
@@ -12,6 +13,15 @@ export const ListOfCategories = () => {
         setCategories(response)
       })
   }, [])
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+  })
 
   const renderList = (fixed) => (
     <List className={fixed ? 'fixed' : ''}>
@@ -24,7 +34,7 @@ export const ListOfCategories = () => {
   return (
     <>
       {renderList()}
-      {renderList(true)}
+      {showFixed && renderList(true)}
     </>
   )
 }
