ec6a11049d035e68401337c6640b2b893dd9ee0b
Javier Montalvo <giojavi04@gmail.com>
2020-02-11 00:31:21 -0500

    render props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import { Logo } from './components/Logo'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { GlobalStyle } from './styles/GlobalStyles'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
-export const App = () => (
-  <>
-    <GlobalStyle />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={2} />
-  </>
-)
+export const App = () => {
+  const urlParms = new window.URLSearchParams(window.location.search)
+  const detailId = urlParms.get('detail')
+
+  return (
+    <>
+      <GlobalStyle />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={2} />
+            </>
+      }
+    </>
+  )
+}
