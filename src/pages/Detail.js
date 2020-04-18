e55c1d6e659da75278e8b3ea293c72a85dade7cc
Javier Montalvo <giojavi04@gmail.com>
2020-04-18 14:52:01 -0500

    React Helmet

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { PhotoCardWithQuery } from '../container/PhotoCardWithQuery'
+import { Layout } from '../components/Layout'
 
 export const Detail = ({ detailId }) => (
-  <PhotoCardWithQuery id={detailId} />
+  <Layout title={`Forografía ${detailId}`}>
+    <PhotoCardWithQuery id={detailId} />
+  </Layout>
 )
