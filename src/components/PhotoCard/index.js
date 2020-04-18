fde0f9e1bfcc7333a40d081b4bb1142bfa47ddf4
Javier Montalvo <giojavi04@gmail.com>
2020-04-17 20:28:18 -0500

    Hacer like como usuario registrado (jwt)

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import { Link } from '@reach/router'
 import { ImgWrapper, Img, Article } from './styles'
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
 
   return (
     <Article ref={element}>
@@ -27,10 +24,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             {
               (toggleLike) => {
                 const handleFavClick = () => {
-                  !liked && toggleLike({ variables: {
-                    input: { id }
-                  } })
-                  setLiked(!liked)
+                  toggleLike({
+                    variables: {
+                      input: { id }
+                    }
+                  })
                 }
                 return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
               }
