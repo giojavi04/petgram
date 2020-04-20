71301b9e75ba879b28d3bd3ab36fdf2704e35516
Javier Montalvo <giojavi04@gmail.com>
2020-04-19 20:20:24 -0500

    Testing con Cypress

---
+++ cypress/integration/petgram/test_spec.js
@@ -0,0 +1,23 @@
+/* global describe, it, cy */
+
+describe('Petgram', function () {
+  it('para ver si la app funciona', function () {
+    cy.visit('/')
+  })
+
+  it('navegamos a una categoría y vemos fotos', function () {
+    cy.visit('/pet/1')
+    cy.get('article')
+  })
+
+  it('si podemos navegar ocn la navbar a la home', function () {
+    cy.visit('/pet/1')
+    cy.get('nav a').first().click()
+    cy.url().should('include', '/')
+  })
+
+  it('los usuarion no registrados ven el formulario de registro e inicio de sesion al ir a favs', function () {
+    cy.visit('/favs')
+    cy.get('form').should('have.length', 2)
+  })
+})
